be16d3adeeb1e3c7a46cc4d96d19a494eef5c416
MaiShelbayeh <mayshelbayeh@gmail.com>
2025-10-07 16:21:49 +0300

    ..

--- src/components/common/CheckboxSpecificationSelector.tsx
+++ src/components/common/CheckboxSpecificationSelector.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { X } from 'lucide-react';
 // import { useTranslation } from 'react-i18next';
+
+
 import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 
 
